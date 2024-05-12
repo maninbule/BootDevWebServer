65a5960d03b24def9d8d3da9393c6a45c7eee95a
maninbule <2373926690@qq.com>
2024-05-12 12:48:33 +0000

    add fileserver

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Print("hello world")
+	port := "8080"
+	mux := http.NewServeMux()
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
